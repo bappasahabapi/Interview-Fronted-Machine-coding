f89fc29bb8f2eb061b9b0baff8af36f931407d20
bappasaha <bappabappa840@gmail.com>
2025-09-06 23:22:59 +0600

    Added TS

---
+++ PS/ts-01-add.ts
@@ -0,0 +1,14 @@
+function addsum(n1: number, n2: number): number {
+    return (n1 + n2);
+}
+
+let ans = addsum(5, 6);
+console.log(ans)
+
+const add = (a: number, b: number): number => {
+    let sum = a + b;
+    return sum;
+}
+
+let test = add(5, 3);
+console.log(test)
